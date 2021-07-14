710f5591381d9b91fbe90b01802b91fcc6b0910f
Milan <milanbulatovic@outlook.com>
2021-07-14 19:29:35 +0200

    Add files via upload

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Color changer
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/MilanBuli/pen/wvdgLBp](https://codepen.io/MilanBuli/pen/wvdgLBp).
+
+
---
+++ src/script.js
@@ -0,0 +1,24 @@
+//Selectors
+const background = document.querySelector('.wrapper');
+const button = document.querySelector('.button');
+const result = document.querySelector('.span');
+
+//Color
+const color = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+
+button.addEventListener('click', function() {
+  let hash = '#';
+  for (i=0; i<6; i++) {
+    hash += color[number()];
+  }
+  
+  background.style.backgroundColor = hash;
+  result.innerText = hash;
+});
+
+function number() {
+  return Math.floor(Math.random() * color.length);
+}
+                       
+   
