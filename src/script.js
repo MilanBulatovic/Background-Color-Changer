511269a872a09780dbc70a8bf28bc075a5701651
Milan <milanbulatovic@outlook.com>
2021-10-15 03:30:45 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -7,18 +7,20 @@ const result = document.querySelector('.span');
 const color = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 
+function colorCode() {
+  return Math.floor(Math.random() * color.length);
+}
+
 button.addEventListener('click', function() {
-  let hash = '#';
+  const hash = '#';
   for (i=0; i<6; i++) {
-    hash += color[number()];
+    hash += color[colorCode()];
   }
   
   background.style.backgroundColor = hash;
   result.innerText = hash;
 });
 
-function number() {
-  return Math.floor(Math.random() * color.length);
-}
+
                        
    
